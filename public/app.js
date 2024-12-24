2db9938fd295a84d0ffea0af75aa275ab008fb8b
Kamogelo <ekmogasoa@gmail.com>
2024-12-24 14:00:24 +0200

    Moved the data variable to the outer scope so it can be accessed inside the event listener

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 // Select the section where menu items will be displayed
 const sectionCenter = document.querySelector(".section-center");
-// Select all filter buttons
-const filterBtns = document.querySelectorAll(".filter-btn");
+// Container where filter buttons will be displayed
+const btnContainer = document.querySelector(".btn-container");
 
 // Function to fetch menu items data from JSON file
 const jsonMenuItemsData = async () => {
@@ -14,27 +14,11 @@ const jsonMenuItemsData = async () => {
     }
     let data = await res.json();
 
-    // Add event listeners to filter buttons
-    filterBtns.forEach((btn) => {
-      btn.addEventListener("click", (evt) => {
-        const category = evt.currentTarget.dataset.id;
-        const h1 = document.querySelector("h1");
-        let menuCategory;
-        if (category === "all") {
-          h1.textContent = "food menu";
-          menuCategory = data;
-        } else {
-          menuCategory = data.filter(
-            (dataMenu) => dataMenu.category === category
-          );
-          h1.textContent = category;
-        }
-        displayMenuItems(menuCategory);
-      });
-    });
-
     // Display all items initially
     displayMenuItems(data);
+
+    // Generate filter buttons
+    displayMenuButtons(data);
   } catch (err) {
     console.log(err);
   }
@@ -60,6 +44,48 @@ const displayMenuItems = (menuItems) => {
   });
   displayMenu = displayMenu.join("");
   sectionCenter.innerHTML = displayMenu;
+};
+
+// Function to display filter buttons
+const displayMenuButtons = (data) => {
+  const categories = data.reduce(
+    (values, item) => {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ["all"]
+  );
+
+  const categoryBtns = categories
+    .map((category) => {
+      return `<button class="filter-btn" type="button" data-id=${category}>${category}</button>`;
+    })
+    .join("");
+  btnContainer.innerHTML = categoryBtns;
+
+  // Select all filter buttons
+  const filterBtns = document.querySelectorAll(".filter-btn");
+
+  // Add event listeners to filter buttons
+  filterBtns.forEach((btn) => {
+    btn.addEventListener("click", (evt) => {
+      const category = evt.currentTarget.dataset.id;
+      const h1 = document.querySelector("h1");
+      let menuCategory;
+      if (category === "all") {
+        h1.textContent = "food menu";
+        menuCategory = data;
+      } else {
+        menuCategory = data.filter(
+          (dataMenu) => dataMenu.category === category
+        );
+        h1.textContent = category;
+      }
+      displayMenuItems(menuCategory);
+    });
+  });
 };
 
 // Call the function to fetch and display menu items
